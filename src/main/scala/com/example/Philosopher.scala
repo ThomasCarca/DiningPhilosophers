643281463f68c946671dbbcc391f451873c5b3da
Unknown <nicolas@nbrun.fr>
2019-01-17 15:03:22 +0100

    Refactoring Philosopher
    New constructor + ADD life, hunger

--- src/main/scala/com/example/Philosopher.scala
+++ src/main/scala/com/example/Philosopher.scala
@@ -2,14 +2,20 @@ package com.example
 
 import akka.actor.Actor
 
-class Philosopher extends Actor {
+class Philosopher(val MAX_LIFE: Int, val MAX_HUNGER: Int, val TIME_TO_EAT: Int) extends Actor {
+
+  var life = MAX_LIFE
+  var hunger = MAX_HUNGER
+
+
   override def receive: Receive = thinking
 
   def thinking: Receive = {
-    case "hungry" =>
-      println("I'm hungry !")
-      Thread.sleep(1000)
-      context.become(starving)
+    case "tictac" =>
+      val e : String = self.path.name
+      hunger = hunger-1
+      println(s"$e => hunger : $hunger ")
+
   }
 
   def eating: Receive = {
