ceb3d313cc72ffbbbf668dff09e934b7f769af25
Unknown <nicolas@nbrun.fr>
2019-01-17 15:04:23 +0100

    Generate multiple philosophers

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -4,15 +4,14 @@ import akka.actor.{ActorRef, ActorSystem, Props}
 
 object Main extends App {
   val system: ActorSystem = ActorSystem("Dinner")
-  val philosopher1: ActorRef = system.actorOf(Props[Philosopher], "philosopher1")
 
-  // hungry
-  philosopher1 ! "hungry"
+  val fork: ActorRef = system.actorOf(Props[Fork])
 
-  // eating
-  philosopher1 ! "eat"
+  val r = scala.util.Random
 
-  // thinking
-  philosopher1 ! "done eating"
+  val philosophers: List[ActorRef] = (1 to 5).map(_ => system.actorOf(Props(new Philosopher(r.nextInt(10)+5,r.nextInt(10)+5, r.nextInt(3)+1)))).toList
+
+
+  (1 to 10).foreach{ _ => println("----------------------------");philosophers.foreach( philosopher =>  philosopher ! "tictac" ); Thread.sleep(2000)}
 
 }
