336838a6a7270831b261e65bd6954a590ac6fd20
Thomas <boulbestho@eisti.eu>
2019-01-19 20:41:03 +0100

    Refactored Main for for better readability - Implemented Philosopher state change and death - Test are broken due to Philosopher behavior changes.

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -4,15 +4,22 @@ import akka.actor.{ActorRef, ActorSystem, Props}
 
 object Main extends App {
 
+  def life: Int = scala.util.Random.nextInt(10) + 5
+
+  def hunger: Int = scala.util.Random.nextInt(10) + 5
+
+  def timeToEat: Int = scala.util.Random.nextInt(3) + 2
+
+  def philosopherProp: Props = Props(new Philosopher(life, hunger, timeToEat))
+
   val system: ActorSystem = ActorSystem("Dinner")
 
   val fork: ActorRef = system.actorOf(Props[Fork])
 
-  val r = scala.util.Random
+  val philosophers: List[ActorRef] = (1 to 5).map(_ => system.actorOf(philosopherProp)).toList
 
-  val philosophers: List[ActorRef] = (1 to 5).map(_ => system.actorOf(Props(new Philosopher(r.nextInt(10)+5,r.nextInt(10)+5, r.nextInt(3)+1)))).toList
-
-
-  (1 to 10).foreach{ _ => philosophers.foreach( philosopher => philosopher ! "tictac"); Thread.sleep(2000)}
-
+  while (true) {
+    philosophers.foreach(philosopher => philosopher ! "tictac")
+    Thread.sleep(500)
+  }
 }
--- src/main/scala/com/example/Philosopher.scala
+++ src/main/scala/com/example/Philosopher.scala
@@ -1,12 +1,13 @@
 package com.example
 
-import akka.actor.Actor
+import akka.actor.{Actor, PoisonPill}
 
 class Philosopher(val MAX_LIFE: Int, val MAX_HUNGER: Int, val TIME_TO_EAT: Int) extends Actor {
 
   var life: Int = MAX_LIFE
   var hunger: Int = 0
-
+  var eatingTurn: Int = 0
+  var numberOfTurnsSurvived: Int = 0
 
   override def receive: Receive = thinking
 
@@ -17,19 +18,36 @@ class Philosopher(val MAX_LIFE: Int, val MAX_HUNGER: Int, val TIME_TO_EAT: Int) 
   def thinking: Receive = {
     case "tictac" =>
       hunger += 1
-      sender() ! "I'm hungry !"
+      numberOfTurnsSurvived +=1
+      if (hunger == MAX_HUNGER) self ! "hungry"
       print()
+    case "hungry" =>
+      println(s"${self.path.name} is now hungry !")
+      context.become(hungry)
   }
 
   def eating: Receive = {
+    case "tictac" =>
+    eatingTurn += 1
+    numberOfTurnsSurvived +=1
+    if (eatingTurn == TIME_TO_EAT) self ! "done eating"
+    print()
     case "done eating" =>
+      println(s"${self.path.name} is back to thinking !")
       context.become(thinking)
   }
 
   def hungry: Receive = {
+    case "tictac" =>
+    life -= 1
+    numberOfTurnsSurvived += 1
+    if (life == 0) {
+      println(s"${self.path.name} just died... He survived $numberOfTurnsSurvived turns.")
+      self ! PoisonPill
+    }
+    print()
     case "eat" =>
+    println(s"${self.path.name} is now eating !")
     context.become(eating)
   }
-
-  override def unhandled(message: Any): Unit = sender() ! "Mmmh ?"
 }
